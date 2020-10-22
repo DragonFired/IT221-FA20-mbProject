a7ab1af53897491afa7a5bc0eb2cb88fa8da338a
Arana Fireheart <a.fireheart@snhu.edu>
2020-10-21 20:38:23 -0400

    Add posts app, tests, models, views and templates

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn mbProject.wsgi --log-file -
--- mbProject/settings.py
+++ mbProject/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'n9-p7l-na136tz*o5-^)a9r)9pek%6ycf2r$v25y^ys(oi$b--'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
